3a3d879e926af48cdde407e12acf410f28a6e6ea
DESKTOP-CK5NU32\INTEL <742047848@qq.com>
2021-09-25 17:27:38 +0800

    新修改,数据类型改变。结果正常

--- main.cpp
+++ main.cpp
@@ -2,37 +2,35 @@
 #include <thread>
 #include "RuntimeMeasure.hpp"
 
-void sumlong(long start, long end, long &ans)
+void sumlong(size_t start, size_t end, size_t &ans)
 {
-    long s = 0;
+    size_t s1 = 0;
     for (long i = start; i < end; ++i)
     {
-        s += i;
+        s1 += i;
     }
-    ans = s;
+    ans = s1;
 }
-
-const long s = 1000000000;
-
+const size_t  s = 1000000000;
 int main()
 {
     TimeMeasure([]()
                 {
-                    long ans1, ans2, ans3;
-                    std::thread t1 = std::thread(sumlong, 0, s / 3, std::ref(ans1));
-                    std::thread t2 = std::thread(sumlong, s / 3, (s * 2) / 3, std::ref(ans2));
-                    std::thread t3 = std::thread(sumlong, (s*2)/3, s, std::ref(ans3));
+                    size_t  ans1, ans2;
+                    std::thread t1(sumlong, 1, s / 2, std::ref(ans1));
+                    std::thread t2(sumlong, s / 2, s, std::ref(ans2));
                     t1.join();
                     t2.join();
-                    t3.join();
-                    std::cout << "3 threads: " << (ans1 + ans2 + ans3) << std::endl;
+                    std::cout << (ans1 + ans2) << std::endl;
                 });
 
     TimeMeasure([]()
                 {
-                    long ans4;
-                    sumlong(0, s, std::ref(ans4));
-                    std::cout << "1 threads: " << ans4 << std::endl;
+                    size_t ans3;
+                    sumlong(0, s, std::ref(ans3));
+                    std::cout << "1 threads: " << ans3 << std::endl;
                 });
+    std::cout<<sizeof(size_t)<<std::endl;
+    std::cout<<sizeof(long long)<<std::endl;
     return 0;
 }
